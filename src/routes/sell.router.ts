18a49d28d26db2d78bd0b7b94d2207d71fe5ef5f
root <root@azelada.localdomain>
2022-12-20 01:34:43 -0400

    imp: log number

--- src/routes/sell.router.ts
+++ src/routes/sell.router.ts
@@ -3,6 +3,7 @@ export default function SellRoute(router: Router) {
 
     router.get('/', (req: Request, res: Response) => {
         const num = randomNumber(5);
+        console.log(num);
         res.json({
             count: num
         });
